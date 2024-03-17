9fc34c234b8f4e7328b9c8a597cce772b920bb08
sourabh2104 <sourabhyadav1256@gmail.com>
2024-03-18 00:12:07 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'signLanguages',
     version= '0.0.0',
-    author= 'iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Sourabh',
+    author_email= 'sourabhyadav1256@gmail.com',
     packages= find_packages(),
     install_requires = []
 
